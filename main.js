46bf05571abcdbdd46d9287dffb75a95668163cf
leoTav <65578088+Leon1618@users.noreply.github.com>
2022-05-23 22:40:17 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log(Math.floor(3/2));
+console.log(Math.sin(3.5));
+console.log(Math.PI);
+console.log(3,141592653589793);
+console.log(Math.PHI);
+
+const r = "1";
+const circumference = 2 * Math.PI * r;
+console.log(circumference);
+
+console.log(parseInt("10.2abc" ));
+console.log(parseFloat("10.2abc" ));
+
+console.log(+'0x10');
+
+
